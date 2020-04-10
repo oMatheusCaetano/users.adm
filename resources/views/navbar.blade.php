b91c7c67a69dc2948e04be469556c17fce056533
Matheus Caetano <caetanomatheus876@gmail.com>
2020-04-10 16:07:08 -0300

    Arrumando o dropdown da navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ asset('img/logo.png') }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>@yield('window_title')</title>
 </head>
@@ -18,45 +19,47 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link text-dark" href="{{ route('users.index') }}">Home</a>
-                        </li>
-                        @auth
+                    <div class="collapse navbar-collapse justify-content-between col-7" id="navbarNav">
+                        <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link text-dark" href="#">Novo Usuário</a>
+                                <a class="nav-link" href="{{ route('users.index') }}">Home</a>
                             </li>
-                        @endauth
-                    </ul>
+                            @auth
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Novo Usuário</a>
+                                </li>
+                            @endauth
+                        </ul>
+                    @auth
+                        <div class="dropdown show">
+                            <a class="text-secondary dropdown-toggle text-info" href="#" role="button" id="userDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {{ $logedUser->name }}
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="#">Perfil</a>
+                                <a class="dropdown-item text-danger" href="{{ route('login.logout') }}">Sair</a>
+                            </div>
+                        </div>
+                    @endauth
+                    @guest
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link text-info" href="{{ route('login.index') }}">Entrar</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark" href="#">Registrar-se</a>
+                            </li>
+                        </ul>
+                    @endguest
                 </div>
-                @auth
-                <div class="dropdown show">
-                    <a class="text-secondary dropdown-toggle" href="#" role="button" id="userDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      {{ $logedUser->name }}
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="userDropdown">
-                        <a class="dropdown-item" href="#">Perfil</a>
-                        <a class="dropdown-item text-danger" href="{{ route('login.logout') }}">Sair</a>
-                    </div>
-                </div>
-                @endauth
-                @guest
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link text-info" href="{{ route('login.index') }}">Entrar</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-dark" href="#">Registrar-se</a>
-                    </li>
-                </ul>
-                @endguest
             </div>
         </nav>
     </header>
+
     <section class="container">
         @yield('page_content')
     </section>
+
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
